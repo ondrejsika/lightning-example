079e9dcd3f90cdc6503ef1dd81f25a48e7b82c1e
Ondrej Sika <ondrej@ondrejsika.com>
2018-04-06 22:15:20 +0200

    Add bitcoin regtest

---
+++ bitcoin/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z $CHAIN ]; then
+    echo "Variable CHAIN is not set"
+    exit 1
+fi
+
+echo Configured for "$CHAIN"
+# create default configuration based on selected network (chain)
+cp /root/bitcoin-"$CHAIN".conf /root/.bitcoin/bitcoin.conf
+
+# workarround for regtest case
+if [ $CHAIN = "regtest" ]; then
+    echo "workarround for regtest"
+    (sleep 10; bitcoin-cli generate 1) &
+fi
+
+echo "Parameters for bitcoind command are (if any):" $@
+bitcoind $@
+
